aa3d12deb347f5b5cc31eb46b44f4bc76e68eab2
Ken Ling <ken88ling@gmail.com>
2018-11-05 23:36:53 +0800

    redux basic flow

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "SUBSTRACT":
+      state = state - action.payload;
+      break;
+  }
+  return state;
+};
+
+const store = createStore(reducer, 100);
+
+store.subscribe(() => {
+  console.log("store updated ", store.getState());
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 101
+});
+
+store.dispatch({
+  type: "SUBSTRACT",
+  payload: 20
+});
+
 serviceWorker.unregister();
