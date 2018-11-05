6429f1bbd5f60e7571b58cad4c7b7bdae4471154
Ken Ling <ken88ling@gmail.com>
2018-11-06 00:14:14 +0800

    redux flow - object

--- src/index.js
+++ src/index.js
@@ -8,19 +8,34 @@ import * as serviceWorker from "./serviceWorker";
 
 import { createStore } from "redux";
 
-const reducer = (state, action) => {
+const initialState = {
+  result: 1,
+  lastValues: [],
+  username: "Ken"
+};
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD":
-      state = state + action.payload;
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload] // this immutable way
+      };
+      //state.lastValue.push(action.payload); // this is multable way
       break;
     case "SUBSTRACT":
-      state = state - action.payload;
+      state = {
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload]
+      };
+      //state.lastValue.push(action.payload);
       break;
   }
   return state;
 };
 
-const store = createStore(reducer, 100);
+const store = createStore(reducer);
 
 store.subscribe(() => {
   console.log("store updated ", store.getState());
@@ -28,12 +43,16 @@ store.subscribe(() => {
 
 store.dispatch({
   type: "ADD",
-  payload: 101
+  payload: 100
 });
 
 store.dispatch({
   type: "SUBSTRACT",
-  payload: 20
+  payload: 10
 });
 
+store.dispatch({
+  type: "SUBSTRACT",
+  payload: 15
+});
 serviceWorker.unregister();
