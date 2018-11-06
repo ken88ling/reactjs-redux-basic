602faa4f3d6dc11fa80238caa071b8c88a21a29e
Ken Ling <ken88ling@gmail.com>
2018-11-06 23:01:24 +0800

    add async

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import User from "../components/User";
 import Main from "../components/Main";
 import { connect } from "react-redux";
+import { setName } from "../actions/userActions";
 
 class App extends Component {
   render() {
@@ -20,13 +21,11 @@ const mapStateToProps = state => {
     math: state.mathReducer
   };
 };
+
 const mapDispatchToProps = dispatch => {
   return {
     setName: name => {
-      dispatch({
-        type: "SET_NAME",
-        payload: name
-      });
+      dispatch(setName(name));
     }
   };
 };
