96703f63dfd2ea08c9d230a871da7ffca1f32bea
Ken Ling <ken88ling@gmail.com>
2018-11-07 21:03:28 +0800

    update

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,11 +1,25 @@
+import { resolve } from "path";
+import { reject } from "rsvp";
+
+// export function setName(name) {
+//   return dispatch => {
+//     setTimeout(() => {
+//       dispatch({
+//         type: "SET_NAME",
+//         payload: name
+//       });
+//     }, 2000);
+//   };
+// }
+
 export function setName(name) {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch({
-        type: "SET_NAME",
-        payload: name
-      });
-    }, 2000);
+  return {
+    type: "SET_NAME",
+    payload: new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(name);
+      }, 2000);
+    })
   };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import mathReducer from "./reducers/userReducer";
 import userReducer from "./reducers/userReducer";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import promise from "redux-promise-middleware";
 
 const store = createStore(
   combineReducers({ mathReducer, userReducer }),
-  composeWithDevTools()
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
 
 export default store;
